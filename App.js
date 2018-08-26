9023b472ac52e081f36a8d3c2f155af24cfbcec3
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2018-08-26 23:51:54 +0900

    Added boilerplate code for Firebase

--- App.js
+++ App.js
@@ -2,9 +2,22 @@ import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
-import reducers from './src/reducers'
+import reducers from './src/reducers';
+import firebase from 'firebase';
 
 export default class App extends React.Component {
+
+  componentWillMount(){
+    const config = {
+      apiKey: "AIzaSyBxp_Y3FZOmiIM7RfHAYDVGZwZBYQ-9eaU",
+      authDomain: "authentication-afe67.firebaseapp.com",
+      databaseURL: "https://authentication-afe67.firebaseio.com",
+      projectId: "authentication-afe67",
+      storageBucket: "authentication-afe67.appspot.com",
+      messagingSenderId: "913075465416"
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
