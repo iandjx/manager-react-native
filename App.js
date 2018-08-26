8022b019aab3d7ff7f3588d2a8edca8673438b49
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2018-08-26 23:40:53 +0900

    Added boilerplate code for redux

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
+import reducers from './src/reducers'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
