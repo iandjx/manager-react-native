d562482ba79ec68001da121534264f81571de377
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2018-08-28 06:44:56 +0900

    Added Loginform

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import { StyleSheet, Text, View } from 'react-native';
-import reducers from './src/reducers';
-import firebase from 'firebase';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { StyleSheet, Text, View } from "react-native";
+import reducers from "./src/reducers";
+import firebase from "firebase";
+import LoginForm from "./src/components/LoginForm";
 
 export default class App extends React.Component {
-
-  componentWillMount(){
+  componentWillMount() {
     const config = {
       apiKey: "AIzaSyBxp_Y3FZOmiIM7RfHAYDVGZwZBYQ-9eaU",
       authDomain: "authentication-afe67.firebaseapp.com",
@@ -21,11 +21,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
