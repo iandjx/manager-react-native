f3adee2384ae96c1cdc92936c28d4ec364529a9b
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2018-08-29 06:34:24 +0900

    Created Action creater and implemented it

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import { Card, CardSection, Input, Button } from "./common";
+import { emailChanged } from "../actions";
+import { connect } from "react-redux";
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
   render() {
     return (
       <Card>
         <CardSection>
-          <Input label="Email" placeholder="email@gmail.com" />
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChange={this.onEmailChange.bind(this)}
+          />
         </CardSection>
 
         <CardSection>
@@ -21,4 +31,7 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+export default connect(
+  null,
+  { emailChanged }
+)(LoginForm);
