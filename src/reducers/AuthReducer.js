810e2d7d808feaea1f70ce7927ee59c790eeeab1
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2018-08-31 07:05:02 +0900

    Refactored login actions and reducers

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,8 +1,13 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from "../actions/types";
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
+} from "../actions/types";
 
 const INITIAL_STATE = {
   email: "",
-  password: ""
+  password: "",
+  user: null
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -12,6 +17,8 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload };
     default:
       return state;
   }
