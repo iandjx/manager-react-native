adbe4e5a5a552d2f2e434802d968cce6aa5f2a51
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2018-09-01 06:20:40 +0900

    Implemented login failed and new user creation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from "./types";
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
+} from "./types";
 import firebase from "firebase";
 
 export const emailChanged = text => {
@@ -20,8 +25,21 @@ export const loginUser = ({ email, password }) => {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+      .then(user => loginUserSucess(dispatch, user))
+      .catch(() => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSucess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
       });
   };
+};
+
+const loginUserSucess = (dispatch, user) => {
+  dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+};
+
+const loginUserFail = dispatch => {
+  dispatch({ type: LOGIN_USER_FAIL });
 };
