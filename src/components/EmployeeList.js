63c5f19c11782b4756ef3fc8b8d3af1125068664
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2018-09-06 22:08:52 +0900

    Convert employees object to array

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { View, Text } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { employeeFetch } from "../actions";
+import _ from "lodash";
 
 class EmployeeList extends Component {
   componentWillMount() {
@@ -9,18 +10,27 @@ class EmployeeList extends Component {
   }
 
   render() {
+    console.log(this.props);
     return (
       <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
+        <Text>test</Text>
       </View>
+      // <FlatList
+      //   data={this.props.employees}
+      //   renderItem={({ item }) => <ListItem employee={item} />}
+      // />
     );
   }
 }
 
+const mapStateToProps = state => {
+  //transform Object state.employees to Array
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid }; //{ shift: 'Monday', name: 'Vinh', id: '123213' }
+  });
+  return { employees };
+};
 export default connect(
-  null,
+  mapStateToProps,
   { employeeFetch }
 )(EmployeeList);
